9087f06e8aba2f97774d0dd167c657c5ca7dc459
EricCR85 <ecr.ceo@gmail.com>
2026-08-16 23:11:57 -0400

    Fix footer links for LInkedIn, Github, and Resume PDF

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,56 @@
-import React from 'react'
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
     <footer>
-        <div className="footer__content">
-            <div className="footer__logocontainer">
-                <a href="#" className="footer__anchor">
-                    <img src={`${process.env.PUBLIC_URL}/assets/footerlogo.png`} className="footer__logo" alt="footer logo"></img>
-                    <span className="footer__logo__popper">
-                        Top
-                        <i className="fas fa-arrow-up"></i>
-                    </span>
-                </a>
-            </div>
-            <ul className="footer__social-links">
-                <li className="footer__social-link">
-                    <a href="https://linkedin.com/in/dvdhyelee/" target="_blank">LinkedIn</a>
-                </li>
-                <li className="footer__social-link">
-                    <a href="https://github.com/DvdHLee" target="_blank">GitHub</a>
-                </li>
-                <li className="footer__social-link">
-                    <a href="assets/resume.pdf" target="_blank">Resume</a>
-                </li>
-            </ul>
-            <p className="copyright">Copyright © 2026 Eric Reeves</p>
+      <div className="footer__content">
+        <div className="footer__logocontainer">
+          <a href="#" className="footer__anchor">
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/footerlogo.png`}
+              className="footer__logo"
+              alt="footer logo"
+            />
+            <span className="footer__logo__popper">
+              Top
+              <i className="fas fa-arrow-up"></i>
+            </span>
+          </a>
         </div>
+        <ul className="footer__social-links">
+          <li className="footer__social-link">
+            <a
+              href="https://www.linkedin.com/in/eric-reeves-36ab5514b/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li className="footer__social-link">
+            <a
+              href="https://github.com/EricCR85"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </li>
+          <li className="footer__social-link">
+            <a
+              href={`${process.env.PUBLIC_URL}/resume.pdf`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+        <p className="copyright">Copyright © 2026 Eric Reeves</p>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
